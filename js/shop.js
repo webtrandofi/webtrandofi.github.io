82cb8cc934b6ce61282917f27ef2c37c8f4bf827
Your Name <you@example.com>
2015-02-23 15:52:40 +0200

    ok

--- js/shop.js
+++ js/shop.js
@@ -1,17 +1,12 @@
 $(document).ready(function(){
     $.ajax({
-        url:  "//api.tumblr.com/v2/blog/lakiasiaintoimistotrast.tumblr.com/posts/text?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4&limit=3&filter=text",
+        url:  "//api.tumblr.com/v2/blog/trandofi.tumblr.com/posts/text?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4",
         dataType: 'jsonp',
         success: function(results){
             console.log(results);
             results.response.posts.forEach(function(post){
-                post_html = '<div class="col-md-4"><article class="experience"><header><h3>' + post.title + '</h3></header><p>' + post.body.substring(0, 200);
-                // if (post.boby.length > 200) {
-                //    post_html = post_html  + '...'; 
-                // }
-                post_html = post_html  + '...'; 
-                post_html = post_html + '<br><a href="' + post.post_url + '" target="_blank">More</a></p></article></div>';
-                $("#tumblr-posts").append(post_html);
+                post_html = '<div class="span4"><h3>' + post.title + '</h3>' + post.body + '</div>';
+                $("#varaosakauppa_content").append(post_html);
             });
         }
     });
